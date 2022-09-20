d11cf4ba0b160c966c094742cfa2eb3c147dba17
Fabricio <tekendefabri@gmail.com>
2022-09-20 22:20:57 +0200

    Game main function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,40 @@
-// alert("Working");
+function start(){
+    const container = document.getElementById("container");
+    const gameContainer = document.getElementById("gameContainer");
+    
+    container.style.display = "none";
+    gameContainer.removeAttribute("hidden");
+}
+
+function game(userOption){
+    const machineOptions = ["rock", "paper", "scissors"];
+    const numRandom = Math.floor(Math.random() * 3);
+    const machineOption = machineOptions[numRandom];
+
+    console.log(`Me: ${userOption}\nMachine:${machineOption}`)
+    if(userOption === machineOption){
+        tie();
+    }else if(userOption === "rock" && machineOption === "paper"){
+        lose();
+    }else if(userOption === "rock" && machineOption === "scissors"){
+        win();
+    }else if(userOption === "paper" && machineOption === "rock"){
+        win();
+    }else if(userOption === "paper" && machineOption === "scissors"){
+        lose();
+    }else if(userOption === "scissors" && machineOption === "rock"){
+        lose();
+    }else if(userOption === "scissors" && machineOption === "paper"){
+        win();
+    }
+}
+
+function tie(result){
+    alert("tie");
+}
+function win(result){
+    alert("win");
+}
+function lose(result){
+    alert("lose");
+}
