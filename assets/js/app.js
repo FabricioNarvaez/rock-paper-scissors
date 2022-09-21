58fe96969c4f941f8809af23d161bb8642d6b741
Fabricio <tekendefabri@gmail.com>
2022-09-21 20:03:09 +0200

    Created score container

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,17 +11,33 @@ const imageSource = {
     scissors : "assets/images/scissors.png"
 }
 
+const scores = {
+    you: 0,
+    machine: 0
+}
+
 const containerStart = document.getElementById("containerStart");
 const gameContainer = document.getElementById("gameContainer");
+const scoreContainer = document.getElementById("scoreContainer");
 
+// Result constants
 const resultContainer = document.getElementById("resultGroup");
 const resultText = document.getElementById("resultText");
 const yourOptionIMG = document.getElementById("yourOption");
 const machineOptionIMG = document.getElementById("machineOption");
 
+// Score constants
+const yourScore = document.getElementById("yourScore");
+const machineScore = document.getElementById("machineScore");
+
+// Functions
 function start(){
     containerStart.style.display = "none";
     gameContainer.removeAttribute("hidden");
+    scoreContainer.removeAttribute("hidden");
+    console.log(scores["you"])
+    yourScore.innerHTML = `${scores["you"]}`;
+    machineScore.innerHTML = `${scores["machine"]}`;
 }
 
 function game(userOption){
